24f7ad5446d168b947cfea3b41ccf007a9d2e53d
Felipe-Moura <41745265+Felipe-Moura@users.noreply.github.com>
2021-07-05 17:57:54 -0300

    Concertando

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const db = require('./dbConect');
+
+const app = express();
+
+app.get('/all', async (req, res) => {
+    const result = await db.query("SELECT * FROM usuario");
+    console.table(result.rows);
+
+    res.send({"rows": result.rows});
+});
+
+app.listen(3000, () => {
+    console.log(`escutando a porta 3000`);
+});
